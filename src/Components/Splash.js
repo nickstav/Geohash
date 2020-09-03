057cf55f2a28445b040995aa8e73cbba3dc760c9
nickstav <47791300+nickstav@users.noreply.github.com>
2020-09-03 19:03:14 +0100

    splash page edit

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -5,6 +5,7 @@ function Splash() {
         <div id="intro">
             <h2>Welcome to the world of Geohashing!</h2>
             <p>Please select an option from the menu above</p>
+            <a href="https://en.wikipedia.org/wiki/Geohash" target="_blank">(what is Geohashing?)</a>
         </div>
     )
 }
